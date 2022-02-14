fa0a03acca1dc8232f4c3c9b2ca1a819f37fc05b
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-02-14 19:56:23 -0300

    converting arrow functions

--- modules/displayScreen.js
+++ modules/displayScreen.js
@@ -1,32 +1,30 @@
 // onclick Lis
 /* eslint-disable no-unused-vars */
-function displayList() {
+const displayList = () => {
   document.getElementById('book-table').style.display = 'flex';
   document.getElementById('new-book').style.display = 'none';
   document.getElementById('contact').style.display = 'none';
   document.getElementById('dList').style.color = '#04aa6d';
   document.getElementById('dAddBook').style.color = 'black';
   document.getElementById('dContact').style.color = 'black';
-}
-
+};
 // onclick Add Book
 
-function displayAddBook() {
+const displayAddBook = () => {
   document.getElementById('book-table').style.display = 'none';
   document.getElementById('new-book').style.display = 'flex';
   document.getElementById('contact').style.display = 'none';
   document.getElementById('dList').style.color = 'black';
   document.getElementById('dAddBook').style.color = '#04aa6d';
   document.getElementById('dContact').style.color = 'black';
-}
-
+};
 // onclick Contact
 
-function displayContact() {
+const displayContact = () => {
   document.getElementById('book-table').style.display = 'none';
   document.getElementById('new-book').style.display = 'none';
   document.getElementById('contact').style.display = 'block';
   document.getElementById('dList').style.color = 'black';
   document.getElementById('dAddBook').style.color = 'black';
   document.getElementById('dContact').style.color = '#04aa6d';
-}
+};
